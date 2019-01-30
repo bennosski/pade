f80aec85b5174607e4508864493f7e388d867489
Ben <bln@stanford.edu>
2019-01-29 23:22:25 -0800

    fixed example

--- example.py
+++ example.py
@@ -7,16 +7,14 @@ xs = linspace(0.1, 5, 10)
 zs = linspace(0, 20, 20)*1j
 us = f(zs)
 
+
 print('original f')
-print(['%1.4f %1.4f'%(f(x).real, f(x).imag) for x in f(xs)])
+print(['%1.4f %1.4f'%(x.real, x.imag) for x in f(xs)])
 
 p = pade.fit(zs, us)
 
 print('\npade approximant')
-print(['%1.4f %1.4f'%(p(x).real, p(x).imag) for x in xs])
+print(['%1.4f %1.4f'%(x.real, x.imag) for x in p(xs)])
 
 print('\ndifference')
 print(['%1.4f %1.4f'%(f(x).real-p(x).real, f(x).imag-p(x).imag) for x in xs])
-
-
-print(p(xs))
